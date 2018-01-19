8a8b3ec5e5c2a082716063aa47383ac52785c8c1
admin <vishmithakv@gmail.com>
2018-01-19 16:41:22 +0530

    plz check

---
+++ app/src/main/java/com/example/admin/registrationpage/User.java
@@ -0,0 +1,100 @@
+package com.example.admin.registrationpage;
+
+import java.util.Date;
+
+/**
+ * Created by admin on 19-Jan-18.
+ */
+
+public class User {
+    private int id;
+    private String fname;
+    private String mname;
+    private String lname;
+    private String email;
+    private String password;
+    private int phno;
+    private int faxno;
+    private Date DOB;
+    private String Address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getMname() {
+        return mname;
+    }
+
+    public void setMname(String mname) {
+        this.mname = mname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPhno() {
+        return phno;
+    }
+
+    public void setPhno(int phno) {
+        this.phno = phno;
+    }
+
+    public int getFaxno() {
+        return faxno;
+    }
+
+    public void setFaxno(int faxno) {
+        this.faxno = faxno;
+    }
+
+    public Date getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(Date DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+}
